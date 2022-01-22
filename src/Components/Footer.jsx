8c29cb3377bb4f17581f7b9057db974b758dc390
Oloyede Adeosun <oloyedeadeosun@Oloyedes-MacBook-Air.local>
2022-01-22 15:29:06 +0000

    icon-styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiOutlineFacebook } from "react-icons/ai";
 import { FaTwitterSquare } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
 
-const footerIconStyles = { color: "red", fontSize: "1.5rem" };
+const footerIconStyles = { color: "#918e9b", fontSize: "2.5rem" };
 
 export default function Footer({ footerIconStyles }) {
   return (
