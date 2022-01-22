0ee9d79d62ec3eff579c7a0064d83e2d5b5e2141
Oloyede Adeosun <oloyedeadeosun@Oloyedes-MacBook-Air.local>
2022-01-22 15:41:25 +0000

    icon-styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,11 +7,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="icons">
-        <AiFillGithub
-          color=" #918e9b"
-          font-size="1.5rem"
-          margin="margin: 8px;"
-        />
+        <AiFillGithub color=" #918e9b" font-size="1.5rem" margin="8px" />
         <FaTwitterSquare className="twitter" />
         <AiOutlineFacebook className="facebook" />
       </div>
