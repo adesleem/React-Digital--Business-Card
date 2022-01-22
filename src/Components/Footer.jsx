5450170547540c816b90cfdb7340089def128fc2
Oloyede Adeosun <oloyedeadeosun@Oloyedes-MacBook-Air.local>
2022-01-22 15:24:46 +0000

    icon-styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,13 @@ import { AiOutlineFacebook } from "react-icons/ai";
 import { FaTwitterSquare } from "react-icons/fa";
 import { AiFillGithub } from "react-icons/ai";
 
-export default function Footer() {
+const footerIconStyles = { color: "red", fontSize: "1.5rem" };
+
+export default function Footer({ footerIconStyles }) {
   return (
     <div className="footer">
       <div className="icons">
-        <AiFillGithub className="github" />
+        <AiFillGithub style={footerIconStyles} />
         <FaTwitterSquare className="twitter" />
         <AiOutlineFacebook className="facebook" />
       </div>
